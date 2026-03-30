7b631343767b0c8a4fc8418332db1aaf25f221be
chloebyun-wd <chloe.byun@workday.com>
2026-03-30 09:23:14 -0700

    feat: add config for custom page/tab title (#359)
    * feat: add config for custom page/tab title
    
    * rebase

--- dist/components/Bot.d.ts
+++ dist/components/Bot.d.ts
@@ -86,6 +86,7 @@ export type observersConfigType = Record<'observeUserInput' | 'observeLoading' |
 export type BotProps = {
     chatflowid: string;
     apiHost?: string;
+    pageTitle?: string;
     onRequest?: (request: RequestInit) => Promise<void>;
     chatflowConfig?: Record<string, unknown>;
     backgroundColor?: string;
